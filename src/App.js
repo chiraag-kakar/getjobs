8db917dbf5f6b8a932be3d54016a606923686e1f
chiraag-kakar <chirucos1998@gmail.com>
2020-09-27 00:19:13 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap'
 import Job from './Job'
 import JobsPagination from './JobsPagination';
 import SearchForm from './SearchForm';
+import ParticlesBg from 'particles-bg'
 
 function App() {
   const [params, setParams] = useState({})
@@ -21,7 +22,8 @@ function App() {
 
   return (
     <Container className="my-4">
-      <h1 className="mb-4">GitHub Jobs</h1>
+      <ParticlesBg type="square" bg={true} />
+      <center><h1 className="mb-4">Get Jobs using GitHub Jobs API</h1></center>
       <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -6,11 +6,11 @@ export default function SearchForm({ params, onParamChange }) {
     <Form className="mb-4">
       <Form.Row className="align-items-end">
         <Form.Group as={Col}>
-          <Form.Label>Description</Form.Label>
+          <Form.Label>Job Description</Form.Label>
           <Form.Control onChange={onParamChange} value={params.description} name="description" type="text" />
         </Form.Group>
         <Form.Group as={Col}>
-          <Form.Label>Location</Form.Label>
+          <Form.Label>Job Location</Form.Label>
           <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" />
         </Form.Group>
         <Form.Group as={Col} xs="auto" className="ml-2">
