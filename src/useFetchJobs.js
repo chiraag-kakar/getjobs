7f39c78a8fb3843a32080f500816bccf15a5325b
Chiraag Kakar <chirucos1998@gmail.com>
2020-10-15 22:50:14 +0530

    Update useFetchJobs.js

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -8,7 +8,7 @@ const ACTIONS = {
   UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
 }
 
-const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
+const BASE_URL = 'https://jobs.github.com/positions.json'
 
 function reducer(state, action) {
   switch (action.type) {
